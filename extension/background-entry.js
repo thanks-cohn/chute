1610d2f357c0a3854fdf1fc228c00fe635cb7dec
thanks-cohn <officialmahasbiz@gmail.com>
2026-08-21 16:19:58 -0500

    Load custom storage compatibility layer

--- extension/background-entry.js
+++ extension/background-entry.js
@@ -1,6 +1,7 @@
 importScripts(
   "background.js",
   "background-provenance.js",
+  "background-custom-storage-compat.js",
   "background-context-resolver.js",
   "background-context-menu.js",
   "background-sidepanel.js"
